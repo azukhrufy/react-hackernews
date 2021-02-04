27b45f1b1388af4b9865b9fdec36a7796e585f6e
azukhrufy <azukhrufy@outlook.com>
2021-02-04 16:59:58 +0700

    feat(getStoryId): using useEffect to get all storyIds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getStoryIds} from '../src/services/hnApi';
 
 export const App = () => {
+  const [storyIds, setStoryIds] = useState();
+
+  useEffect(() => {
+    getStoryIds().then(result => setStoryIds(result));
+    // console.log(getStoryIdeas());
+  }, []);
+  
   return (
     <div>
-      <p>Hello World</p>
+      <p>{JSON.stringify(storyIds)}</p>
     </div>
   )
 }
