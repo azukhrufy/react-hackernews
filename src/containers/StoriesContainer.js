c9b6b79d828f1befa956c27cfa2a9221e2ed61ea
azukhrufy <azukhrufy@outlook.com>
2021-02-08 16:04:25 +0700

    feat(components): add Story Components, adding StoryId as a props to the components

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {getStoryIds, getStory} from '../services/hnApi';
+import { Story } from '../components/Story';
 
 export const StoriesContainer = () => {
-    const [storyIds, setStoryIds] = useState();
+    const [storyIds, setStoryIds] = useState([]);
   
     useEffect(() => {
       getStoryIds().then(result => setStoryIds(result));
@@ -10,9 +11,5 @@ export const StoriesContainer = () => {
       // console.log(getStoryIdeas());
     }, []);
     
-    return (
-      <div>
-        <p>{JSON.stringify(storyIds)}</p>
-      </div>
-    )
+    return storyIds.map(storyId => <Story storyId={storyId} /> );
   };
