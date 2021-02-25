b41c0f57f2ea8e4ff23ca055959a697c54f05668
azukhrufy <azukhrufy@outlook.com>
2021-02-25 14:41:45 +0700

    feat(infinite-scroll):add infinite scroll for rendering story

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { getStoryIds } from '../services/hnApi';
 import { Story } from '../components/Story';
 import { StoriesContainerWrapper, GlobalStyle } from '../styles/StoriesContainerStyles';
+import { useInfiniteScroll } from "../hooks/useInfiniteScroll";
 
 export const StoriesContainer = () => {
+	const {count} = useInfiniteScroll();
 	const [storyIds, setStoryIds] = useState([]);
+	
 
 	useEffect(() => {
 		getStoryIds().then((result) => setStoryIds(result));
@@ -15,7 +18,7 @@ export const StoriesContainer = () => {
 			<GlobalStyle />
 			<StoriesContainerWrapper data-test-id="stories-container">
 				<h1>Hacker News Stories</h1>
-				{storyIds.map((storyId) => (
+				{storyIds.slice(0,count).map((storyId) => (
 					<Story key={storyId} storyId={storyId} />
 				))}
 			</StoriesContainerWrapper>
